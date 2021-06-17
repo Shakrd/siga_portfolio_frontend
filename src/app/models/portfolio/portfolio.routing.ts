04d63c340b16cd3379f33dade5148458e8e69e74
Marcelo Duque <30637651+Marcduk@users.noreply.github.com>
2021-06-16 23:02:57 -0500

    porrtfolio works

--- src/app/models/portfolio/portfolio.routing.ts
+++ src/app/models/portfolio/portfolio.routing.ts
@@ -1,15 +1,14 @@
-import { AuthGuard } from './../shared/guards/auth.guard';
+import { AuthGuard } from 'src/app/shared/guards/auth.guard';
 import { PortfolioComponent } from './portfolio.component';
 import { PortfolioModule } from './portfolio.module';
 import {Routes} from '@angular/router';
 
 
 
+
 export const PortfolioRoutes: Routes = [
     {
-        path: 'portfolio',
-        component: PortfolioComponent,
-        canActivate: [AuthGuard]
+        
         
     }
 ];
