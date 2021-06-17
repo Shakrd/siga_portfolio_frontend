103bac1d2443c6770ec5e272a5dc518e53051817
Shakrd <jvc.vega@yavirac.edu.ec>
2021-06-17 12:38:07 -0500

    first commit98

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,39 +25,19 @@ import { PortfolioComponent } from './pages/portfolio/portfolio.component';
                         path: 'dashboard',
                         loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule),
                         canActivate: [AuthGuard]
-<<<<<<< HEAD
                     }, */ 
-=======
-                    },
-<<<<<<< HEAD
-                    /*{
-                        path: 'user',
-                        loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule),
-                        canActivate: [AuthGuard]
-                    }*/
-=======
->>>>>>> main
                     /* {
                         path: 'pea',
                         loadChildren: () => import('./pages/portfolio/portfolio.module').then(m => m.PortfolioModule),
                         canActivate: [AuthGuard]
-<<<<<<< HEAD
                     } */
-=======
-<<<<<<< HEAD
-                    } */
->>>>>>> 66d87adf4bdf40cd447e452dcdd9ca0bfb8229f8
-=======
-                    } 
->>>>>>> 1f52e1f6c05c76e3eaef80240249ad34748ee4dd
->>>>>>> main
                 ]
             },
-            /* {
+             {
                 path: 'pea-web',
                 component: AppBlankComponent,
                 loadChildren: () => import('./pages/portfolio/portfolio.module').then(m => m.PortfolioModule)
-            }, */
+            }, 
              /* {
                 path: 'auth',
                 component: AppBlankComponent,
